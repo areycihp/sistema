7e1bd9c7fac5733b1c747299a3d82f8d981bb180
Areyci <areyci1997@hotmail.com>
2025-08-27 18:15:22 -0600

    feat: Adding views for CRUD (static data)

--- libreria/urls.py
+++ libreria/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'), #Lleva el view de Inicio
     path('nosotros', views.nosotros, name='nosotros'),
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear'),
+    path('libros/editar', views.editar, name='editar'),
 ] 
--- libreria/views.py
+++ libreria/views.py
@@ -5,7 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Bienvenido bro") #Imprime solo el texto
+    # return HttpResponse("Bienvenido bro") #Imprime solo el texto
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html') #Busca el HTML
+
+def libros(request):
+    return render(request, 'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
+def editar(request):
+    return render(request, 'libros/editar.html')
